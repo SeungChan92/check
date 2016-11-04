b9907e0f0ff02a3cfa07ff7e58d08c1fb13cbd1a
Seung-Chan Ahn <issea1015@gmail.com>
2016-11-04 16:21:01 +0900

    make base of version 3

--- src/IT/java/all/Test.java
+++ src/IT/java/all/Test.java
@@ -1,47 +1,23 @@
-package com.daou.verification;
+package all;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-import com.daou.verification.config.Config;
-import com.daou.verification.service.module.Common;
-import com.daou.verification.service.module.Member;
-import com.daou.verification.service.module.Message;
-import com.daou.verification.service.module.Tool;
-import com.daou.verification.service.suite.BasicSuite;
-
-public class Main {
+public class Test {
+	private WebDriver webDriver = null;
 	
-	private static WebDriver webDriver = null;
-	
-	public static void main(String[] args) {
-        setup();
-        verify();
-        finish();
-    }
-
-	// depth 1
-	private static void setup() {
-		setup_webDriver();
-		setup_modules();
-		setup_config();
-	}
-	private static void verify() {
-		BasicSuite.construct();
-		BasicSuite.run();
-	}
-	private static void finish() {
+	@org.junit.Test
+	public void test() {
+		this.setup_webDriver();
+		webDriver.get("http://www.naver.com");
 		webDriver.quit();
 	}
-	
-	// depth 2
-	private static void setup_webDriver() {
+	private void setup_webDriver() {
 		
 //		// # make
 //		// ## [for] local test
@@ -64,14 +40,5 @@ public class Main {
 		webDriver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 		webDriver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
 		webDriver.manage().window().maximize();
-	}
-	private static void setup_modules() {
-		Tool.setWebDriver(webDriver);
-		Common.setWebDriver(webDriver);
-		Member.setWebDriver(webDriver);
-		Message.setWebDriver(webDriver);
-	}
-	private static void setup_config() {
-		Config.load();
 	}
 }
--- src/main/java/com/daou/verification/service/module/Common.java
+++
@@ -1,9 +0,0 @@
-package com.daou.verification.service.module;
-
-import com.daou.verification.config.Config;
-
-public class Common extends Module {
-	public static void goToPage_home() {
-		driver.get(Config.get("baseUrl"));
-	}
-}
