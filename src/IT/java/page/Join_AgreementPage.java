491d3a993fdbc2d28dcb1e1bab520b0f9c04421c
Seung-Chan Ahn <issea1015@gmail.com>
2016-11-11 11:12:58 +0900

    clean

--- src/IT/java/page/Join_AgreementPage.java
+++ src/IT/java/page/Join_AgreementPage.java
@@ -4,8 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-import infra.Config;
-
 public class Join_AgreementPage extends Page {
 	
 	public Join_AgreementPage(WebDriver driver) {
--- src/IT/java/page/Page.java
+++ src/IT/java/page/Page.java
@@ -5,15 +5,15 @@ import org.openqa.selenium.WebDriver;
 import infra.Config;
 
 public class Page {
-	
-	protected final WebDriver driver;
-	
-	public Page(WebDriver driver) {
-        this.driver = driver;
 
-        // Check that we're on the right page.
-        if (!(Config.get("baseUrl") + Config.get_url("this.getClass().getName()")).equals(this.driver.getCurrentUrl())) {
-            throw new IllegalStateException("This is not the " + this.getClass().getName());
-        }
-    }
+	protected final WebDriver driver;
+
+	public Page(WebDriver driver) {
+		this.driver = driver;
+
+		// Check that we're on the right page.
+		if (!(Config.get("baseUrl") + Config.get_url("this.getClass().getName()")).equals(this.driver.getCurrentUrl())) {
+			throw new IllegalStateException("This is not the " + this.getClass().getName());
+		}
+	}
 }
